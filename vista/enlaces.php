a3e9181abcd554f717a445f1f710ad77ef08f285
angelin1219 <66751754+angelin1219@users.noreply.github.com>
2020-06-12 14:28:10 -0500

    v4
    administracion de pestañas

---
+++ vista/enlaces.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/enlaces.css">
+    <title>Document</title>
+</head>
+<body>
+
+    <h1 class="titu">CONSULTAS</h1>
+
+    <div class="contenedor">
+
+        <a href="sub_ambiente.phtml" class="BotonIngreso">Ubicaciòn</a>
+        <a href="sub_usuario.phtml" class="BotonIngreso">Usuarios</a>
+        <a href="sub_ficha.phtml" class="BotonIngreso">Fichas</a>
+        <a href="sub_R_usuario.phtml" class="BotonIngreso">Reporte por usuario</a>
+        <a href="sub_R_ficha.phtml" class="BotonIngreso">Reporte por ficha</a>
+        <a href="sub_R_ambiente.phtml" class="BotonIngreso">Reporte por ambiente</a>
+
+    </div>
+
+</body>
+</html>
